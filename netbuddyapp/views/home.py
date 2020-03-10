93276d835deea4da0e3e4910d2c4b705f34c5667
Trey Suiter <trey.suiter@gmail.com>
2020-03-09 20:24:55 -0500

    uptime diplaying on home page

--- netbuddyapp/views/home.py
+++ netbuddyapp/views/home.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
+from netmiko import ConnectHandler
 
 def home(request):
 	
     if request.method == 'GET':
+        device = {}
+        device['device_type'] = 'cisco_ios'
+        device['ip'] = '172.16.1.1'
+        device['username'] = 'admin'
+        device['password'] = 'adminpass1'
+        conn = ConnectHandler(**device)
+        output = conn.send_command("show version | i uptime")
         template = 'home.html'
-        context = {}
+        context = {'output': output}
 
         return render(request, template, context)

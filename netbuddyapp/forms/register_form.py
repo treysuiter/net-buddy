140699f060a30b4a5c203d5d591de3cdd6f0346b
Trey Suiter <trey.suiter@gmail.com>
2020-03-12 07:44:41 -0500

    new user able to register

---
+++ netbuddyapp/forms/register_form.py
@@ -0,0 +1,17 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class RegisterForm(UserCreationForm):
+    # first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
+    # last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
+    # email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+    current_router_ip = forms.CharField(max_length=55)
+    current_vlan_ip = forms.CharField(max_length=55)
+    ssh_username = forms.CharField(max_length=55)
+    ssh_password = forms.CharField(max_length=55)
+
+    class Meta:
+        model = User
+        fields = ('username', 'current_router_ip', 'current_vlan_ip', 'ssh_username', 'ssh_password','password1', 'password2', )
--- netbuddyapp/views/auth/register.py
+++ netbuddyapp/views/auth/register.py
@@ -2,16 +2,22 @@ from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 
+from netbuddyapp.forms.register_form import RegisterForm
+
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = RegisterForm(request.POST)
         if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
+            user = form.save()
+            user.netbuddyuser.current_router_ip = form.cleaned_data.get('current_router_ip')
+            user.netbuddyuser.current_vlan_ip = form.cleaned_data.get('current_vlan_ip')
+            user.netbuddyuser.ssh_username = form.cleaned_data.get('ssh_username')
+            user.netbuddyuser.ssh_password = form.cleaned_data.get('ssh_password')
+            user.save()
             raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
+            user = authenticate(username=user.username, password=raw_password)
             login(request, user)
             return redirect('netbuddyapp:routerconfiglist')
     else:
-        form = UserCreationForm()
+        form = RegisterForm()
     return render(request, 'registration/register.html', {'form': form})

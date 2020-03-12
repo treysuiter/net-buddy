32b08ba86fa2c29c96dd00bf2c5d0f592e1b11d1
Trey Suiter <trey.suiter@gmail.com>
2020-03-11 19:06:03 -0500

    can list router config files

--- netbuddyapp/urls.py
+++ netbuddyapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from netbuddyapp import views
-from .views import home
+from .views import *
 
 app_name = 'netbuddyapp'
 
 urlpatterns = [
     path('', home, name='home'),
+    path('routerconfiglist/', router_config_list, name='router_config_list')
 ]
--- netbuddyapp/views/__init__.py
+++ netbuddyapp/views/__init__.py
@@ -1,1 +1,4 @@
+#Views imports
+
 from .home import home
+from .router.routerconfig_list import router_config_list

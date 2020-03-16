3d58e29be7c602f38187a5aacbf20343de281ed8
Trey Suiter <trey.suiter@gmail.com>
2020-03-16 09:27:28 -0500

    got js working

---
+++ netbuddyapp/static/scripts/loadingMessage.js
@@ -0,0 +1,8 @@
+// const infoDialog = document.querySelector(".loading-message")
+console.log("javascript working")
+
+// document.querySelector(".router-create-form").addEventListener("submit", () => {
+//     console.log("this ran")
+//     infoDialog.show()
+//     }
+// )
--- netbuddyproject/settings.py
+++ netbuddyproject/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles'
+    
 ]
 
 MIDDLEWARE = [
@@ -118,6 +119,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/netbuddyapp/static/'
+STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = '/routerconfiglist'

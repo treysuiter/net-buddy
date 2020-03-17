a16bc30ba6855380a2d7d5332b17c6b3368382e1
Trey Suiter <trey.suiter@gmail.com>
2020-03-17 14:54:22 -0500

    mass formatting change; underline nav working if file is selcteded or in edit mode

--- netbuddyapp/static/scripts/loadingMessage.js
+++ netbuddyapp/static/scripts/loadingMessage.js
@@ -1,15 +1,15 @@
-// const loadingMessage = document.querySelector(".loading-message")
+const loadingMessage = document.querySelector(".loading-message")
 
-// document.querySelectorAll(".router-create-form").addEventListener("submit", () => {
-//     loadingMessage.show()
-//     }
-// )
-
-document.querySelector(".router-create-form").addEventListener("submit", (evt) => {
-    if (evt.target.id.startsWith("show")) {
-        infoDialog.show()
+document.querySelector(".router-create-form").addEventListener("submit", () => {
+    loadingMessage.show()
     }
-})
+)
+
+// document.querySelector(".router-create-form").addEventListener("submit", (evt) => {
+//     if (evt.target.id.startsWith("show")) {
+//         infoDialog.show()
+//     }
+// })
 
 // const loadingMessage = document.querySelector(".loading-message")
 

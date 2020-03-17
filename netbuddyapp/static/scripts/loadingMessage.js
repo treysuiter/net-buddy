c09615cc814a17843a000362ff3521067bf66af4
Trey Suiter <trey.suiter@gmail.com>
2020-03-17 11:03:02 -0500

    working on router commands; need load nessage on all buttons; removed console  log from loadmessage.js

--- netbuddyapp/static/scripts/loadingMessage.js
+++ netbuddyapp/static/scripts/loadingMessage.js
@@ -2,7 +2,6 @@ const loadingMessage = document.querySelector(".loading-message")
 console.log("javascript working")
 
 document.querySelector(".router-create-form").addEventListener("submit", () => {
-    console.log("this ran")
     loadingMessage.show()
     }
 )
--- netbuddyapp/views/router/router_commands.py
+++ netbuddyapp/views/router/router_commands.py
@@ -22,14 +22,67 @@ def router_commands(request):
             "actual_method" in form_data
             and form_data["actual_method"] == "SHOW_IP_INTERFACES_BRIEF"
         ):
-            conn = ConnectHandler(**device)
-            output = conn.send_command('show ip interface brief')
-            template = 'router/router_commands.html'
-            context = {
-                'output': output
-            }
+            try:
+                conn = ConnectHandler(**device)
+                output = conn.send_command('show ip interface brief')
+                template = 'router/router_commands.html'
+                context = {
+                    'output': output
+                }
+                conn.disconnect()
+                return render(request, template, context)
 
-            return render(request, template, context)
+            except Exception as exception:
+
+                error_text='Uh oh, looks like something went wrong. Check and see is your device is running, connected, and configured properly.'
+                template = 'router/router_current_info.html'
+                context = {'error_text': error_text, 'exception': exception}
+
+                return render(request, template, context)
+
+        if (
+            "actual_method" in form_data
+            and form_data["actual_method"] == "SHOW_IP_ROUTE"
+        ):
+            try:
+                conn = ConnectHandler(**device)
+                output = conn.send_command('show ip route')
+                template = 'router/router_commands.html'
+                context = {
+                    'output': output
+                }
+                conn.disconnect()
+                return render(request, template, context)
+
+            except Exception as exception:
+
+                error_text='Uh oh, looks like something went wrong. Check and see is your device is running, connected, and configured properly.'
+                template = 'router/router_current_info.html'
+                context = {'error_text': error_text, 'exception': exception}
+
+                return render(request, template, context)
+
+        if (
+            "actual_method" in form_data
+            and form_data["actual_method"] == "SHOW_VERSION"
+        ):
+            try:
+                conn = ConnectHandler(**device)
+                output = conn.send_command('show version')
+                template = 'router/router_commands.html'
+                context = {
+                    'output': output
+                }
+                conn.disconnect()
+                return render(request, template, context)
+
+            except Exception as exception:
+
+                error_text='Uh oh, looks like something went wrong. Check and see is your device is running, connected, and configured properly.'
+                template = 'router/router_current_info.html'
+                context = {'error_text': error_text, 'exception': exception}
+
+                return render(request, template, context)           
 
         else:
             

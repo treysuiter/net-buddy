8b06d52d55a9ca329633b8cb47b88a9cce1f061e
Trey Suiter <trey.suiter@gmail.com>
2020-03-18 14:13:36 -0500

    changed class for loading message for forms; created navloadingmessage.js

---
+++ netbuddyapp/static/scripts/navLoadingMessage.js
@@ -0,0 +1,9 @@
+const navLoadingMessage = document.querySelector(".navLoadingMessage")
+
+
+document.querySelector(".navList").addEventListener("click", (evt) => {
+    if (evt.target.id("current-router-info-link")) {
+        navLoadingMessage.show()
+    }
+}
+)
